215bf19b5465985cee6cfea7b0d43d925c9a3b15
Mohamed Emara <mohamedemara33@gmail.com>
2026-08-13 02:29:47 +0300

    feat: add AboutUs component with company statistics and interactive image gallery

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -231,11 +231,9 @@ export default function AboutUs() {
                   height="100%"
                   src="https://www.youtube.com/embed/vZeOXQzMbuU?autoplay=1"
                   title="YouTube video player"
-                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
-                  className="h-full w-full"
+                  className="h-full w-full border-0"
                 ></iframe>
               </div>
             </motion.div>
